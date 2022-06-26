3362c6ab5a2d171a66173fe2bda595d5e88a2139
jortan02 <jortan2002@gmail.com>
2022-06-26 00:03:21 -0600

    Added sample transactions, began working on input

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses_app/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +15,16 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: "t2",
+        title: "Weekly Groceries",
+        amount: 16.53,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +32,8 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
               width: double.infinity,
@@ -30,8 +44,54 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
             Card(
-              child: Text("LIST OF TX"),
+              child: 
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(children: [
+                  TextField(),
+                  TextField()
+                ]),
+              ),
             )
+            ,
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text("\$${tx.amount}",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple)),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text(
+                            DateFormat("yyyy-MM-dd").format(tx.date),
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Color.fromARGB(255, 48, 48, 48)),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
           ],
         ));
   }
